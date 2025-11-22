2bbf354e92ba6741aa4990f3a18fde07993b5093
mishabary <mbtrud@mail.ru>
2025-11-22 15:05:39 +0300

    xii step, add new code

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
+import typer
+
+def main(
+    name: str,
+    lastname: str = typer.Option("", help="Фамилия пользователя."),
+    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+):
+    """
+    Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
+    """
+    if formal:
+        print(f"Добрый день, {name} {lastname}!")
+    else:
+        print(f"Привет, {name}!")
+
+if __name__ == "__main__":
+    typer.run(main)
+
+
 """print ("Hello    appsec   world")
 
 for i in [  "Hello" ,   "appsec", "world"   ]:
@@ -11,7 +30,8 @@ run()
 
 #what does dirty code mean?
 #does it mean that the code is not formatted correctly?
-"""
+
 name = input("Enter your name: ")
 print(f"Hello appsec world from @{name}")
 
+"""
