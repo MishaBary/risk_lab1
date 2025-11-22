570151c2a6ae7238f9f3657c1452469e000007ee
mishabary <90476421+MishaBary@users.noreply.github.com>
2025-11-22 15:36:12 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #smth
 import typer
 
-def main(
+def main( ##
     name: str,
     lastname: str = typer.Option("", help="Фамилия пользователя."),
     formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
