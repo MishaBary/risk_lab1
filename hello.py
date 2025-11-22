52191fbfbe71792292e55f53d828d3d65e17dd75
mishabary <mbtrud@mail.ru>
2025-11-22 14:54:22 +0300

    add hello.py with dirty code

---
+++ hello.py
@@ -0,0 +1,13 @@
+print ("Hello    appsec   world")
+
+for i in [  "Hello" ,   "appsec", "world"   ]:
+        print(i),
+
+import sys
+def run():
+	name="appsec"
+	sys.stdout.write("Hello "+ name +" world")
+run()
+
+#what does dirty code mean?
+#does it mean that the code is not formatted correctly?
